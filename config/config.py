edace1e73171893c1485415d48d7c7004fd993d0
ruihexianling <linjiang_xian@yeah.net>
2019-04-26 11:27:29 +0800

    修改config

--- config/config.py
+++ config/config.py
@@ -2,8 +2,6 @@
 
 url1_1 = 'http://192.168.2.7:8082' #测试服务器 app接口
 url1_2 = 'http://192.168.2.7:8088' #测试服务器 后台接口
-url2_1 = 'https://api0.ahqyc.com'   #生产 app
-url2_2 = 'https://api9.ahqyc.com'   #生产 后台
 
 url = url1_1
 
