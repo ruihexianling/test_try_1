e43cc3c70a2d6f07024bb05afa9ee3e397cdda3d
ruihexianling <linjiang_xian@yeah.net>
2019-04-30 16:18:13 +0800

    修改config

--- config/config.py
+++ config/config.py
@@ -1,12 +1,6 @@
-
-
+# url地址配置
 url1_1 = 'http://192.168.2.7:8082' #测试服务器 app接口
 url1_2 = 'http://192.168.2.7:8088' #测试服务器 后台接口
 url2_1 = 'api9.ahqyc.com'
-
 url = url1_1
-
-
 headers = {"Content-Type":"application/json"}
-
-
